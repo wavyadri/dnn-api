0064e58579401f06191d53aab267ccadf53e67bf
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2023-05-12 15:46:08 -0400

    fix cors error

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import news from './routes/news';
 import 'dotenv/config';
 
 const app = express();
+app.use(cors());
 
 app.get('/', (_req, res) => {
   res.send('welcome to Digital Nomad News');
